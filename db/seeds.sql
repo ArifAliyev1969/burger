7dcbcfad4d1fee29e98d02e926d1f9b2349875c1
Hazar <arif.aliyev@gmail.com>
2018-08-07 23:06:42 -0500

    Homework

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ('Cheesburger');
+INSERT INTO burgers (burger_name) VALUES ('Angus');
+INSERT INTO burgers (burger_name) VALUES ('Barbecue');
+INSERT INTO burgers (burger_name) VALUES ('Bacon Cheesburger');
+INSERT INTO burgers (burger_name) VALUES ('Buffalo');
