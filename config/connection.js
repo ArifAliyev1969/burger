4a7b3fc48381bb02b79eec43f42d336f0ce84ab9
Hazar <arif.aliyev@gmail.com>
2018-08-08 20:09:11 -0500

    Homework

--- config/connection.js
+++ config/connection.js
@@ -16,6 +16,23 @@ if(process.env.JAWSDB_URL) {
  
 };
 
+//var connection = mysql.createConnection({
+  //  host: "localhost",
+  //  port: 3306,
+  //  user: "root",
+  //  password: "root",
+  //  database: "cat_db"
+  //});
+  
+  // Make connection.
+  //connection.connect(function(err) {
+    //if (err) {
+      //console.error("error connecting: " + err.stack);
+      //return;
+    //}
+    //console.log("connected as id " + connection.threadId);
+  //});
+
 
 
 connection.connect();
